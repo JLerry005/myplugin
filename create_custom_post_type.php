005175d760c133659f893fb205e66f88969f3ef5
JLerry05 <jlerry005@gmail.com>
2023-03-16 22:33:04 +0800

    first commit

---
+++ create_custom_post_type.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Create your custom post type and taxonomy here.
+ * add a wordpress function to create your custom post type, you can use any name.
+ */
+
+// CUSTOM POST TYPE
+
+function chart_custom_post_type () {
+
+    $labels = array (
+        'name' => 'Chart',
+        'singular_name' => 'Chart',
+        'add_new' => 'Add Item',
+        'all_items' => 'All Items',
+        'edit_item' => 'Edit Item',
+        'new_item' => 'New Item',
+        'view_item' => 'View Item',
+        'search_item' => 'Search Chart',
+        'not_found' => 'No items found',
+        'not_found_in_trash' => 'No items found in trash',
+        'parent_item_colon' => 'Parent Item'
+    );
+
+    $args = array (
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'publicy_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'supports' => array (
+            'title',
+            'editor',
+            'thumbnail',
+            'revisions',
+            'comments'
+        ),
+        'menu_position' => 5,
+        'exclude_from_search' => false,
+        'show_ui' => true,
+        'menu_icon' => 'dashicons-chart-pie'
+    );
+    register_post_type( 'chart', $args );
+}
+
+add_action('init','chart_custom_post_type');
+
+// TAXONOMY
+
+function chart_custom_taxonomies() {
+
+    // Category Taxonomy
+
+    $labels = array (
+        'name' => 'Types',
+        'singular_name' => 'Type',
+        'search_items' => 'Search Types',
+        'all_items' => 'All Types',
+        'parent_item' => 'Parent Type',
+        'parent_item_colon' => 'Parent Type:',
+        'edit_item' => 'Edit Type',
+        'update_item' => 'Update Type',
+        'add_new_item' => 'Add New Chart Type',
+        'new_item_name' => 'New Type Name',
+        'menu_name' => 'Types'
+    );
+
+    $args = array (
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'type' )
+    );
+
+    register_taxonomy( 'type' , array('chart'), $args);
+
+
+    // Post Tag Taxonomy
+
+    register_taxonomy('keyword', 'chart', array (
+        'label' => 'Keywords',
+        'rewrite' => array( 'slug' => 'keyword' ),
+        'hierarchical' => false
+    ) );
+
+}
+
+add_action('init','chart_custom_taxonomies');
---
+++ templates/single-my_custom_post_type.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+ * Template Name: Custom Post Type Template
+ *
+ * This is a custom template for displaying content from a custom post type.
+ * Note: don't forget to add the comment section!!!
+ */
+
+<div class="main-container">
+
+    </?php
+
+    if( have_posts() )
+        while( have_post() ): the_post(); ?>
+            <p><?php the_content(); ?></p>
+            <h3><?php the_title(); ?></h3>
+            <hr>
+            <?php if( has_post_thumbnail() ): ?>
+                <div> <?php the_post_thumbnail('thumbnail'); ?> </div>
+            <?php endif; ?>
+            <small><?php 
+                $terms_list = wp_get_post_terms($post->ID, 'type'); 
+
+                $i = 0;
+                foreach( $terms_list as $term) { $i++;
+                    if( $i > 1 ){echo ', ';}
+                    echo $term->name;
+                }
+            ?> || 
+            <?php 
+                $terms_list = wp_get_post_terms($post->ID, 'keyword'); 
+
+                $i = 0;
+                foreach( $terms_list as $term) { $i++;
+                    if( $i > 1 ){echo ', ';}
+                    echo $term->name;
+                }
+            ?> </small>
+        <?php endwhile;
+    endif;
+    ?>
+
+    <!-- Comment Section -->
+    <?php 
+        if( comments_open() ){ 
+            comments_template(); 
+        } else {
+            echo '<h5 class="text-center"> Sorry, comments is not available right now! </h5>';
+        }
+    ?>
+
+</div>
